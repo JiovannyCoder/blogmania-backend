c988937f3e9fccbef64479df307e6a8ad60d9d3a
JiovannyCoder <haritina110602@gmail.com>
2023-11-29 08:35:50 +0330

    User model created and MongoDB connection established correctly

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
+// mongoose
+const mongoose = require('mongoose')
+
 // express app
 const app = express()
 
 // cors config
 const corsOptions = {
-    origins: ['http://localhost:5173','http://localhost:3000']
+    origins: ['http://localhost:5173', 'http://localhost:3000']
 }
 
 // middlewares
@@ -18,7 +21,11 @@ app.use(express.json())
 
 // routes
 app.get('/', (req, res) => {
-    res.json({message: "Welcome to blog Mania !"})
+    res.json({ message: "Welcome to blog Mania !" })
 })
 
-app.listen(process.env.PORT, () => console.log('The App is open running on port :', process.env.PORT))
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => console.log('The App is open running on port :', process.env.PORT))
+    })
+    .catch(err => console.log(err))
