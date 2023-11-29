c61f73763b032f8b30e677e6ae6ebe02d90915a8
JiovannyCoder <haritina110602@gmail.com>
2023-11-29 08:29:13 +0330

    Server, cors and middlewares configured correctly

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+// express app
+const app = express()
+
+// cors config
+const corsOptions = {
+    origins: ['http://localhost:5173','http://localhost:3000']
+}
+
+// middlewares
+app.use(cors(corsOptions))
+app.use(morgan('dev'))
+app.use(express.json())
+
+// routes
+app.get('/', (req, res) => {
+    res.json({message: "Welcome to blog Mania !"})
+})
+
+app.listen(process.env.PORT, () => console.log('The App is open running on port :', process.env.PORT))
